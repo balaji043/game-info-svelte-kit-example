e63a6372c7be459f091aca067fc575c9046e0794
Balaji R <cubanhog.royalgamma@gmail.com>
2021-12-15 21:02:51 +0530

    removed imports

--- src/routes/store/discover.svelte
+++ src/routes/store/discover.svelte
@@ -1,8 +1,6 @@
 <script>
-import GameSliderSection from '$lib/component/store/GameSliderSection.svelte';
-
+	import GameSliderSection from '$lib/component/store/GameSliderSection.svelte';
 	import StoreCarousel from '$lib/component/store/StoreCarousel.svelte';
-	import { ON_SALE_GAMES } from '$lib/utils/data';
 </script>
 
 <div class="px-8 flex flex-col gap-8">
